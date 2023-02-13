40149700e12b8888aedbcebd9dc550a293a5fa20
zhaoyinggang <1060401583@qq.com>
2023-02-13 22:13:02 +0800

    feat: 完成简单的renderer逻辑

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,26 @@
+// .eslintrc.js
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-      // 这里增加一行，用于支持后面的测试环境
-      jest: true,
-    },
-    extends: ["airbnb-base"],
-    parserOptions: {
-      ecmaVersion: 13,
-      sourceType: "module",
-    },
-    rules: {
-      // 这里添加一行规则把这条规则隐藏
-      "import/prefer-default-export": 0,
-    },
-  };
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    // 关闭 eslint 的如下功能
+    'import/prefer-default-export': 0,
+    'no-use-before-define': 0,
+    'no-shadow': 0,
+    'no-restricted-syntax': 0,
+    'no-return-assign': 0,
+    'no-param-reassign': 0,
+    'no-sequences': 0,
+    'no-loop-func': 0,
+    'no-nested-ternary': 0,
+  },
+};
--- __tests__/utils.js
+++ __tests__/utils.js
@@ -1,5 +1,32 @@
 export function createDiv() {
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    return div;
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  return div;
+}
+
+// 为DOM节点设置属性
+export function applyAttributes(element, attributes) {
+  for (const [key, value] of Object.entries(attributes)) {
+    // 这里需要把类似 strokeWidth 的属性转换成 stroke-width 的形式
+    // 思路就是将大写字母替成 - + 对应的小写字母的形式
+    // 下面涉及到正则匹配，不太了解的同学可以去下面的链接学习：
+    // https://juejin.cn/post/6844903487155732494
+    const kebabCaseKey = key.replace(/[A-Z]/g, (d) => `-${d.toLocaleLowerCase()}`);
+    element.setAttribute(kebabCaseKey, value);
   }
+}
+
+// 将 child 节点挂载到 parent 节点上面
+export function mount(parent, child) {
+  if (parent) {
+    parent.appendChild(child);
+  }
+}
+
+export function getAttributes(node, keys) {
+  const res = {};
+  for (const key of keys) {
+    res[key] = node.getAttribute(key);
+  }
+  return res;
+}
